e005f0b0f5068bdd251f57936b14751a6f2ac0e7
lofienjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2023-01-08 19:57:55 +0000

    Added Framebuffer class

--- src/me/lofienjoyer/nublada/Nublada.java
+++ src/me/lofienjoyer/nublada/Nublada.java
@@ -1,5 +1,7 @@
 package me.lofienjoyer.nublada;
 
+import me.lofienjoyer.nublada.engine.graphics.framebuffer.ColorFramebuffer;
+import me.lofienjoyer.nublada.engine.graphics.framebuffer.Framebuffer;
 import me.lofienjoyer.nublada.engine.graphics.mesh.QuadMesh;
 import me.lofienjoyer.nublada.engine.graphics.shaders.FboShader;
 import me.lofienjoyer.nublada.engine.scene.IScene;
@@ -9,11 +11,11 @@ import me.lofienjoyer.nublada.engine.log.NubladaLogHandler;
 import me.lofienjoyer.nublada.engine.world.BlockRegistry;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL30;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static org.lwjgl.opengl.GL45.*;
 
 public class Nublada {
 
@@ -22,6 +24,7 @@ public class Nublada {
 
     private final Window window;
     public static long WINDOW_ID;
+    private Framebuffer framebuffer;
 
     private IScene currentScene;
 
@@ -48,51 +51,31 @@ public class Nublada {
         long timer = System.nanoTime();
         float delta = 1f;
 
-        int framebuffer = GL30.glGenFramebuffers();
-        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebuffer);
-
-        int textureColorBuffer = GL30.glGenTextures();
-        GL30.glBindTexture(GL30.GL_TEXTURE_2D, textureColorBuffer);
-        GL30.glTexImage2D(GL30.GL_TEXTURE_2D, 0, GL30.GL_RGB, 640, 360, 0, GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE, 0);
-        GL30.glTexParameteri(GL30.GL_TEXTURE_2D, GL30.GL_TEXTURE_MIN_FILTER, GL30.GL_NEAREST);
-        GL30.glTexParameteri(GL30.GL_TEXTURE_2D, GL30.GL_TEXTURE_MAG_FILTER, GL30.GL_NEAREST);
-        GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL30.GL_TEXTURE_2D, textureColorBuffer, 0);
-        GL30.glBindTexture(GL30.GL_TEXTURE_2D, 0);
-
-        int rbo = GL30.glGenRenderbuffers();
-        GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, rbo);
-        GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL30.GL_DEPTH24_STENCIL8, 640, 360);
-        GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_STENCIL_ATTACHMENT, GL30.GL_RENDERBUFFER, rbo);
-        GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, 0);
-
-        if (GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER) != GL30.GL_FRAMEBUFFER_COMPLETE) {
-            Nublada.LOG.severe("Error while creating framebuffer!");
-        }
-
+        framebuffer = new ColorFramebuffer(640, 360);
         FboShader shader = new FboShader();
         QuadMesh quadMesh = new QuadMesh();
 
         while (window.keepOpen()) {
-            GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebuffer);
-            GL30.glClearColor(0.125f, 0f, 1.0f, 0.5f);
-            GL30.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            GL30.glEnable(GL11.GL_DEPTH_TEST);
-            GL30.glViewport(0, 0, 640, 360);
+            framebuffer.bind();
+            glClearColor(0.125f, 0f, 1.0f, 0.5f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glEnable(GL_DEPTH_TEST);
+            glViewport(0, 0, framebuffer.getWidth(), framebuffer.getHeight());
 
             if (currentScene != null)
                 currentScene.render(delta);
 
-            GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
-            GL30.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            GL30.glClear(GL30.GL_COLOR_BUFFER_BIT);
+            framebuffer.unbind();
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             shader.start();
 
-            GL30.glViewport(0, 0, window.getWidth(), window.getHeight());
-            GL30.glBindVertexArray(quadMesh.getVaoId());
-            GL30.glDisable(GL11.GL_DEPTH_TEST);
-            GL30.glBindTexture(GL11.GL_TEXTURE_2D, textureColorBuffer);
-            GL30.glDrawArrays(GL11.GL_TRIANGLES, 0, 6);
+            glViewport(0, 0, window.getWidth(), window.getHeight());
+            glBindVertexArray(quadMesh.getVaoId());
+            glDisable(GL_DEPTH_TEST);
+            glBindTexture(GL_TEXTURE_2D, framebuffer.getColorTextureId());
+            glDrawArrays(GL_TRIANGLES, 0, 6);
 
             window.update();
 
---
+++ src/me/lofienjoyer/nublada/engine/graphics/framebuffer/ColorFramebuffer.java
@@ -0,0 +1,78 @@
+package me.lofienjoyer.nublada.engine.graphics.framebuffer;
+
+import me.lofienjoyer.nublada.Nublada;
+import org.lwjgl.opengl.GL30;
+
+import static org.lwjgl.opengl.GL45.*;
+
+public class ColorFramebuffer implements Framebuffer {
+
+    private final int id, colorTextureId, rboId;
+
+    private int width, height;
+
+    public ColorFramebuffer(int width, int height) {
+        this.id = glGenFramebuffers();
+        bind();
+
+        this.colorTextureId = glGenTextures();
+        this.rboId = glGenRenderbuffers();
+
+        resize(width, height);
+
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+            Nublada.LOG.severe("Error while creating framebuffer!");
+        }
+
+        unbind();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        glBindTexture(GL_TEXTURE_2D, colorTextureId);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTextureId, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        glBindRenderbuffer(GL_RENDERBUFFER, rboId);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rboId);
+        glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    }
+
+    @Override
+    public void bind() {
+        glBindFramebuffer(GL30.GL_FRAMEBUFFER, id);
+    }
+
+    @Override
+    public void unbind() {
+        glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getColorTextureId() {
+        return colorTextureId;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+}
